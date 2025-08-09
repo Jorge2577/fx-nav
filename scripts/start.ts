2505a15cf9562e7735f52f1bb57b3b0d56e0e642
Jorge2577 <Jorge2577@iqiy.eu.org>
2025-08-10 00:36:08 +0800

    Update start.ts

--- scripts/start.ts
+++ scripts/start.ts
@@ -466,7 +466,7 @@ const main = async () => {
     settings.shortcutTitle ??= ''
     settings.shortcutDocTitle ||= ''
     settings.shortcutDockCount ??= 6
-    settings.shortcutThemeShowWeather ??= true
+    settings.shortcutThemeShowWeather ??= false
     settings.shortcutThemeImages ??= [
       {
         src: backgroundImg,
